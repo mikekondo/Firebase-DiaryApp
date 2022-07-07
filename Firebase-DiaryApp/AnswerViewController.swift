975b7548ee06ac58731fe136df5d609c17b84e4e
mikekondo <foad5969@gmail.com>
2022-07-07 14:57:19 +0900

    日記データ送信完了

--- Firebase-DiaryApp/AnswerViewController.swift
+++ Firebase-DiaryApp/AnswerViewController.swift
@@ -5,10 +5,14 @@
 //  Created by 近藤米功 on 2022/07/06.
 //
 
+// UserDefalutsにAnswersのドキュメントパスを保存
+// Answersにはユーザ名、answerTextView、日付を保存する
+
 import UIKit
 import Firebase
 import FirebaseAuth
 import FirebaseFirestore
+import PKHUD
 
 class AnswerViewController: UIViewController {
 
@@ -22,30 +26,27 @@ class AnswerViewController: UIViewController {
 
     let DB = Firestore.firestore()
 
+    private var answersPath: String = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        if UserDefaults.standard.object(forKey: "nameKey") != nil{
+        if UserDefaults.standard.object(forKey: "nameKey") != nil {
             userName = UserDefaults.standard.object(forKey: "nameKey") as! String
         }
+        answersPath = DB.collection("Answers").document().path
+        answersPath = String(answersPath.dropFirst(8))
+        print("answerPath:",answersPath)
         userNameLabel.text = "\(userName)さんようこそ！"
         loadOdaiText()
     }
 
-    private func loadOdaiText(){
-        odaiDB.getDocument { snapShot, error in
-            if error != nil{
-                return
-            }
-            let data = snapShot?.data()
-            guard let data = data else {
-                return
-            }
-            self.odaiLabel.text = data["odaiText"] as! String
-        }
+    @IBAction func didTapAnswerSendButton(_ sender: Any) {
+        sendAnswerTextView()
+        HUD.flash(.success, delay: 1)
     }
 
     @IBAction func didTapLogoutButton(_ sender: Any) {
@@ -60,6 +61,25 @@ class AnswerViewController: UIViewController {
         catch let error as NSError{
             print(error)
         }
+    }
+
+    private func loadOdaiText(){
+        odaiDB.getDocument { snapShot, error in
+            if error != nil{
+                return
+            }
+            let data = snapShot?.data()
+            guard let data = data else {
+                return
+            }
+            self.odaiLabel.text = data["odaiText"] as? String
+        }
+    }
+
+    private func sendAnswerTextView(){
+        let answerText = answerTextView.text ?? ""
+        DB.collection("Answers").document(answersPath).setData(["answer" : answerText,"userName": userName,"postDate":Date().timeIntervalSince1970])
+        answerTextView.text = ""
     }
 
 }
